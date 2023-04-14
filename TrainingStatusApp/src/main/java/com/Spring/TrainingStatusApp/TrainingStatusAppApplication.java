62c97c8cef3c1fbce3791e4735bcf97d0e076bce
YogeshKarne <Yogesh.Karne@outlook.com>
2023-04-14 15:40:08 +0530

    Version Upgrade
    Version Upgrade

--- TrainingStatusApp/src/main/java/com/Spring/TrainingStatusApp/TrainingStatusAppApplication.java
+++ TrainingStatusApp/src/main/java/com/Spring/TrainingStatusApp/TrainingStatusAppApplication.java
@@ -1,11 +1,9 @@
-package com.Spring.TrainingStatusApp;
+package com.spring.TrainingStatusApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.Spring.TrainingStatusApp")
 public class TrainingStatusAppApplication {
 
 	public static void main(String[] args) {
