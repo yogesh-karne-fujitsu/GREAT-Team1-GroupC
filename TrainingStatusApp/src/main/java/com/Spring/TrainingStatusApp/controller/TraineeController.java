cd6a4a14e9d2e94e3e8952a1edb2c969f56e2da6
ShradhaKale1 <shradha.kale@fujitsu.com>
2023-04-18 09:49:21 +0530

    T008 task
    Changes done

--- TrainingStatusApp/src/main/java/com/Spring/TrainingStatusApp/controller/TraineeController.java
+++ TrainingStatusApp/src/main/java/com/Spring/TrainingStatusApp/controller/TraineeController.java
@@ -1,4 +1,4 @@
-package com.spring.TrainingStatusApp.controller;
+package com.Spring.TrainingStatusApp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
